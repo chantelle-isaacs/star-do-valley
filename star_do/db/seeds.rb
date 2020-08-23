d67951ca2255780c866af737688398812a19b0c7
Chantelle Isaacs <chantelle616@gmail.com>
2020-08-23 15:11:29 -0700

    add seed file for rooms

--- star_do/db/seeds.rb
+++ star_do/db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+rooms = [
+    {
+        name: 'Crafts',
+        reward: 'Bridge Repair'
+    },
+    {
+        name: 'Pantry',
+        reward: 'Greenhouse'
+    },
+    {
+        name: 'Fish Tank',
+        reward: 'Glittering Boulder Removed'
+    },
+    {
+        name: 'Boiler',
+        reward: 'Minecarts Repair'
+    },
+    {
+        name: 'Bulletin Board',
+        reward: 'Friendship'
+    },
+    {
+        name: 'Vault',
+        reward: 'Bus Repair'
+    },
+    {
+        name: 'Abandoned JojaMart',
+        reward: 'Movie Theater'
+    },
+]
+
+rooms.each do |attributes|
+    Room.create attributes
+    puts 'creating room #{attributes}'
+end
